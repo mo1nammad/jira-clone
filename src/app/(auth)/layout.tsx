144ae9fe021eba8df92d176260683b44f91c5203
MammadProMax <mhaagrm2@gmail.com>
2024-10-18 16:10:02 +0330

    add auth route

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+type AuthLayoutProps = {
+   children: React.ReactNode;
+};
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+   return (
+      <main className="bg-neutral-100 min-h-screen">
+         <div className="mx-auto max-w-screen-2xl p-4">
+            <nav className="flex justify-between items-center">
+               <Image src={"/logo.svg"} width={152} height={56} alt="logo" />
+
+               <Button variant={"secondary"}>Sign-up</Button>
+            </nav>
+            <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+               {children}
+            </div>
+         </div>
+      </main>
+   );
+}
